e61c983c215f6dd30deb69a8ac400defa59e93ff
alrvnrs <alishaev1997@mail.ru>
2021-05-17 17:10:26 +0300

    aoaoao

---
+++ WpfApp3/WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SettShopEntities context;
+        public MainWindow()
+        {
+            InitializeComponent();
+            context = new SettShopEntities();
+            DataGridRegistration.ItemsSource = context.Shopinformation.ToList();
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            Button btnEdit = sender as Button;
+            var currentinformation = btnEdit.DataContext as Shopinformation;
+            var win = new WindowSettShop(context, currentinformation);
+            win.ShowDialog();
+
+        }
+
+        private void BtnAddClic_Click(object sender, RoutedEventArgs e)
+        {
+            var newEdit = new Shopinformation();
+            context.Shopinformation.Add(newEdit);
+            WindowSettShop win = new WindowSettShop(context, newEdit);
+            win.ShowDialog();
+            DataGridRegistration.ItemsSource = context.Shopinformation.ToList();
+        }
+
+        private void BtnDeleteClic_Click(object sender, RoutedEventArgs e)
+        {
+            var row = DataGridRegistration.SelectedItem as Shopinformation;
+            if (row == null)
+            {
+                MessageBox.Show("Строка не выбрана");
+                return;
+            }
+            MessageBoxResult result = MessageBox.Show("Вы уверены", "Вопрос", MessageBoxButton.YesNoCancel, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                context.Shopinformation.Remove(row);
+                context.SaveChanges();
+                DataGridRegistration.ItemsSource = context.Shopinformation.ToList();
+            }
+        }
+
+        private void CmbSelectOwner_s_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+                if (CmbSelectOwner_s.SelectedItem == null) return;
+                var currentOwner_s = (Owner_s)CmbSelectOwner_s.SelectedItem;
+                List<Owner_s> ListOwner_s = context.Owner_s.ToList();
+                DataGridRegistration.ItemsSource = ListOwner_s.Where(x => x.FirstName == currentOwner_s).ToList();
+           
+        }
+        
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DataGridRegistration.ItemsSource = context.Shopinformation.ToList();
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
+      
---
+++ WpfApp3/WpfApp3/WindowSettShop.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowSettShop.xaml
+    /// </summary>
+    public partial class WindowSettShop : Window
+    {
+        SettShopEntities context;
+        public WindowSettShop(SettShopEntities context, Shopinformation currentinformation)
+        {
+            InitializeComponent();
+            this.context = context;
+            FirstName.ItemsSource = context.Owner_s.ToList();
+            Name.ItemsSource = context.Shopinformation.ToList();
+
+            this.DataContext = currentinformation;
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (CheckEdit())
+            {
+                context.SaveChanges();
+                this.Close();
+            }
+        
+    }
+        private bool CheckEdit()
+        {
+            var reg = this.DataContext as Shopinformation;
+            if (reg.Name == null)
+            {
+                MessageBox.Show("Владелец не выбран");
+                return false;
+            }
+            if (reg.Name == null)
+            {
+                MessageBox.Show("Название магазина не выбрано");
+                return false;
+            }
+            if (reg.Adress == null)
+            {
+                MessageBox.Show("Адресс не записан");
+                return false;
+            }
+
+            if (reg.Shopphone == null)
+            {
+                MessageBox.Show("Номер магазина не записан");
+                return false;
+            }
+            return true;
+        }
+    }
+}
